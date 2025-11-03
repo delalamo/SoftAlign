9269e64a720c3fa612089c935b9fdb2f300a38bf
delalamo <diego.delalamo@gmail.com>
2025-11-03 04:14:45 -0500

    Adding MODRES from colabfold to allow HETATMs

--- softalign/Input_MPNN.py
+++ softalign/Input_MPNN.py
@@ -2,6 +2,21 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 
+# From colabfold
+MODRES = {'MSE':'MET','MLY':'LYS','FME':'MET','HYP':'PRO',
+          'TPO':'THR','CSO':'CYS','SEP':'SER','M3L':'LYS',
+          'HSK':'HIS','SAC':'SER','PCA':'GLU','DAL':'ALA',
+          'CME':'CYS','CSD':'CYS','OCS':'CYS','DPR':'PRO',
+          'B3K':'LYS','ALY':'LYS','YCM':'CYS','MLZ':'LYS',
+          '4BF':'TYR','KCX':'LYS','B3E':'GLU','B3D':'ASP',
+          'HZP':'PRO','CSX':'CYS','BAL':'ALA','HIC':'HIS',
+          'DBZ':'ALA','DCY':'CYS','DVA':'VAL','NLE':'LEU',
+          'SMC':'CYS','AGM':'ARG','B3A':'ALA','DAS':'ASP',
+          'DLY':'LYS','DSN':'SER','DTH':'THR','GL3':'GLY',
+          'HY3':'PRO','LLP':'LYS','MGN':'GLN','MHS':'HIS',
+          'TRQ':'TRP','B3Y':'TYR','PHI':'PHE','PTR':'TYR',
+          'TYS':'TYR','IAS':'ASP','GPL':'LYS','KYN':'TRP',
+          'CSD':'CYS','SEC':'CYS'}
 
 def _np_norm(x, axis=-1, keepdims=True, eps=1e-8, use_jax=True):
   '''compute norm of vector'''
@@ -41,6 +56,15 @@ def get_pdb(x, chains=None):
     line = line.decode("utf-8","ignore").rstrip()
 
     # if line[:4] == "ATOM" or line[:6] == "HETATM":
+    if line[:6] == "HETATM":
+      ch = line[21:22]
+      if chains is None or ch in chains:
+        resi = line[17:20].strip()
+        if resi in MODRES:
+          new_res = MODRES[resi]
+          # turn HETATM into ATOM and replace modified residue name with canonical one
+          line = "ATOM  " + line[6:]
+          line = line[:17] + new_res.ljust(3) + line[20:]
     if line[:4] == "ATOM":
       ch = line[21:22]
       if chains is None or ch in chains:
